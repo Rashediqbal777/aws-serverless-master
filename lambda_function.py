240af1a684ba0471f2790144ce708d3dd4977d09
Md Rashed Iqbal <149344650+Rashediqbal777@users.noreply.github.com>
2023-12-10 20:09:27 +0600

    DynamoDB-Lambda-S3-.csv

--- lambda_function.py
+++ lambda_function.py
@@ -1,44 +1,28 @@
 import boto3
-from boto3.session import Session
+import csv
 from boto3.dynamodb.conditions import Key, Attr
-import base64
 
+table_name = "D y n a m o D B   T a b l e   N a m e"
+bucket_name= "B u c k e t  N a m e"
+temp_filename='/tmp/test.csv'
 
-kms = boto3.client('kms')  
+s3 = boto3.resource('s3')
 dynamodb = boto3.resource('dynamodb')
-table= dynamodb.Table('T a b l e  n a m e')
-def lambda_handler(event, context):
-    KeyId= 'K M S   K E Y'
-    
-    print("###########event##########")
-    print(event)
-    email=event['email']
-    password=event['password']
-    print(email)
-    print(password)
-    
-    ####KMS encryption#####################
-    enc_result= kms.encrypt(
-    KeyId=KeyId,
-    Plaintext=password)
-    
-    blob=enc_result['CiphertextBlob']
-    #print(f'blob {blob}')
-    final_password = (base64.b64encode(blob))
-    
-    ###putting data on dynamodb
-    response = table.put_item(Item={
-        'email':email,
-        'password':final_password
-    })
-    
-    ##get password from database
-    beforekey = table.get_item(Key={'email':email})
-    print(beforekey)
-    
-    ##decrypt
-    print(beforekey['Item']['password'].value)
-    print(kms.decrypt(CiphertextBlob = base64.b64decode(beforekey['Item']['password'].value))['Plaintext'])
-    print("---")
-    afterkey = kms.decrypt(CiphertextBlob = base64.b64decode(beforekey['Item']['password'].value))['Plaintext']
-    return afterkey
+
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event,context):
+    header = True
+    with open(temp_filename,"w") as output_file:
+        writer = csv.writer(output_file)
+        response = table.scan(FilterExpression=Attr('marks_avg').gte(80)) #retriving the candidate info who got >= 80 marks
+        #print(response['Items'])
+        for i in response['Items']:
+            if header == True:
+                writer.writerow(i.keys())
+                header = False
+                
+            
+            writer.writerow(i.values())
+            #print(i.values()) 
+    s3.Bucket(bucket_name).upload_file(temp_filename,"test.csv")
